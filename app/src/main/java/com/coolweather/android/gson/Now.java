1f939b22a6a576245d835e2c1db36b6c271a85b4
guopeixin <peixin.guo24@gmail.com>
2018-08-14 14:06:37 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/Now.java
@@ -0,0 +1,11 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Now {
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond_txt")
+    public String infomation;
+}
